4250a5c4b04383aa5171917de80d9b8d0dc895e7
MartaOsuchaMazur <jagooo1@wp.pl>
2023-11-03 23:33:57 +0100

    added Category and Actor Classes, relations

---
+++ src/main/java/pl/stormit/moviesdirectory/Category.java
@@ -0,0 +1,24 @@
+package pl.stormit.moviesdirectory;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@JsonIgnoreProperties(value = {"movies"})
+public class Category {
+
+    @Id
+    private UUID id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Movie> movies;
+}
--- src/main/java/pl/stormit/moviesdirectory/Movie.java
+++ src/main/java/pl/stormit/moviesdirectory/Movie.java
@@ -5,6 +5,9 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.util.Set;
 import java.util.UUID;
 @Getter
 @Setter
@@ -14,6 +17,12 @@ public class Movie {
     private String name;
     @Id
     private UUID id; // UUID - losowy ciąg znaków, trudniej go zgadnąć, bezpieczniejszy niż int
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany
+    private Set<Actor>actors;
 
     public Movie() {
         id = UUID.randomUUID();  // wygeneruje id
